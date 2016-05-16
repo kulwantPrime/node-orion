e3cb33e608a9ee7aeaf1d995eeb4f6edcb4cd205
KlwntSingh <android.developercode@gmail.com>
2016-05-16 18:53:50 +0530

    test repo init

--- app.js
+++
@@ -1,9 +0,0 @@
-
-console.log("===========================================================");
-console.log("===========================================================");
-console.log("===========================================================");
-
-var config = require('./ApplicationConstants.js');
-var server = require('./main/server.js');
-
-server.startServer(config.PORT);
