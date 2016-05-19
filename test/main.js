734da6bd4fe8d5c9d386398275ce7b24e4d4222e
KlwntSingh <android.developercode@gmail.com>
2016-05-19 16:03:31 +0530

    test case passed

--- test/main.js
+++ test/main.js
@@ -8,7 +8,7 @@ describe('Array', function() {
     it('should return -1 when the value is not present', function () {
       assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
-	  assert.equal(200, 400);
+    	//assert.equal(200, 400);
     });
   });
 });
